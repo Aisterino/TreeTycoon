07fe866ab66dcec0fa1b47e6ae87e1b8887ce2ff
Aisterino <aistismilieska@gmail.com>
2021-05-07 13:40:37 +0300

    Camera
    Added script for Camera to follow player and deleted old script that I wanted to use for computer controls

---
+++ Monkey Business/Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform playerTransform;
+    private float distance = 10f;
+
+    private void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, 0, -distance);
+    }
+}
